37ae840a8a2d39dfa424fc7013dfea7eaf7e30e9
John Turner <john@Johns-MacBook-Pro.local>
2023-04-28 05:09:04 -0500

    added moon

--- main.js
+++ main.js
@@ -57,6 +57,19 @@ const john = new THREE.Mesh(
 
 scene.add(john);
 
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial( {
+    map: moonTexture,
+    normalMap: normalTexture
+  } )
+);
+
+scene.add(moon);
+
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
